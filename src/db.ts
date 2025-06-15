6b2fe2639f0dd1573ef78e1955cef0356ad33f3d
Ullas raj <ullasraj.mec@gmail.com>
2025-06-15 13:57:28 +0530

    mondob added

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const ConnectDB = async () => {
     try{
-        await mongoose.connect('mongodb+srv://<budget_tracker>:<qMbRP7k57wIU6ehg>@cluster0.q8ursxt.mongodb.net/budget_tracking');
+        await mongoose.connect('mongodb+srv://budget_tracker:qMbRP7k57wIU6ehg@cluster0.q8ursxt.mongodb.net/budget_tracking');
         console.log("Database connected successfully")
     }catch(err){
         console.error(err)
