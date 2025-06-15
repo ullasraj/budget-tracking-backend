5d2288452b9c54040ab3d0ce3cbff9a9482b1ec5
Ullas raj <ullasraj.mec@gmail.com>
2025-06-15 14:19:47 +0530

    url changes

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express()
 
 ConnectDB()
 app.use(cors({
-    origin: '*',
+    origin: 'https://budget-tracking-frontend-udcr.onrender.com/',
     credentials: true                // Allow cookies, headers
 }))
 app.use(express.json());
